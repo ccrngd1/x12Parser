0acb0e48b67c5104a3d001216f9b311df2b6380e
Nick.Lawson <Nick.Lawson@R90LMGLA>
2017-05-19 17:00:24 -0400

    added a small code generator for the segmentCollection stuff

---
+++ CodeGenerator/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator
+{
+    public class V2_Generation_SegmentCollection
+    {
+        public string GenerateSegmentCollections(string SegmentsFile)
+        {
+            List<string> segmentNameList = new List<string>();
+
+            using (var reader = File.OpenText(SegmentsFile))
+            {
+                var readLine = reader.ReadLine();
+
+                while (readLine != null)
+                {
+                    if (readLine.ToLower().Contains("public class"))
+                    {
+                        segmentNameList.Add(readLine.Trim().Split(' ').ToList()[2]);
+                    }
+
+                    readLine = reader.ReadLine();
+                }
+            }
+
+            var sb = new StringBuilder();
+
+
+            string classHeaderPrefix = @"public class ";
+            string classHeaderSuffix = @"Collection : baseSegmentCollection {";
+
+            string ctorPrefix = @"public ";
+            string ctorSuffix = @"Collection() : base(typeof(";
+            string ctorEnding = @")){ }";
+
+            string indexPrefix = @"public ";
+            string indexGetPrefix = @" this[int index]{get { return Segments[index] as ";
+            string indexSetPrefix = @" ;} set{Segments[index]=value;}}}";
+
+            foreach (string segName in segmentNameList)
+            {
+                sb.Append(classHeaderPrefix).Append(segName);
+                sb.Append(classHeaderSuffix).Append(ctorPrefix).Append(segName);
+                sb.Append(ctorSuffix).Append(segName);
+                sb.Append(ctorEnding).Append(indexPrefix).Append(segName);
+                sb.Append(indexGetPrefix).Append(segName).Append(indexSetPrefix);
+                sb.AppendLine(" ");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ UnitTestProject1/CodeGenerationTesting.cs
@@ -0,0 +1,26 @@
+﻿using System.IO; 
+using CodeGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CodeGenerationTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            V2_Generation_SegmentCollection segCollectionGenny = new V2_Generation_SegmentCollection();
+
+            string basePath = @"C:\Users\nick.lawson\Google Drive\Documents\projects\x12Parser\X12.837.5010\Base\v2\";
+
+            string s = segCollectionGenny.GenerateSegmentCollections(
+                basePath + "SegmentsV2.cs");
+
+            using (var fs = new StreamWriter(basePath + "CollectionsRAWV2.cs"))
+            {
+                fs.Write(s);
+            }
+        }
+    }
+}
